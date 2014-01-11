cb90e0a5c3b8f64a6b322cafb81005a9fdf7d465
Keith Irwin <keith@zentrope.com>
2014-01-11 12:28:30 -0800

    Convert interactive test app to core.async.

--- src/zentrope_mq/main.clj
+++ src/zentrope_mq/main.clj
@@ -3,73 +3,41 @@
   ;; This file isn't really any sort of good style. I use it to work
   ;; out ideas or bugs while running in a repl.
   ;;
-  (:import [java.util.concurrent Executors TimeUnit])
-  (:require [zentrope-mq.core :as mq]
-            [clojure.tools.logging :as log]))
+  (:require
+    [clojure.tools.logging :as log]
+    [clojure.core.async :refer [thread alts!! chan timeout close!]]
+    [zentrope-mq.core :as mq]))
 
-(def ^:private exchange "default")
-
-;; ----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 ;; App Daemon functions
-;; ----------------------------------------------------------------------------
-
-(let [lock (promise)]
-  (defn- acquire-lock [] (deref lock))
-  (defn- release-lock [] (deliver lock 'done)))
 
 (defn- shutdown-hook
-  [thread-name runnable]
-  (.addShutdownHook (Runtime/getRuntime)
-                    (doto (Thread. runnable)
-                      (.setName thread-name))))
+  [f]
+  (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
 
-;; ----------------------------------------------------------------------------
-;; Thread convenience functions
-;; ----------------------------------------------------------------------------
-
-(def ^:private threads (atom {}))
-
-(defn- run-daemon
-  [pid runnable interval]
-  (let [e (Executors/newScheduledThreadPool 1)
-        t (.scheduleAtFixedRate e runnable interval interval TimeUnit/SECONDS)]
-    (swap! threads assoc pid e)))
-
-(defn- stop-daemon
-  [pid]
-  (when-let [e (pid @threads)]
-    (.shutdownNow e)
-    (swap! threads dissoc pid)))
-
-;; ----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 ;; Publish stuff
-;; ----------------------------------------------------------------------------
 
+(def ^:private exchange "default")
 (def ^:private send-it (partial mq/publish :test-mq exchange "system.status"))
 
-(def ^:private test-msg (.getBytes "{\"app\" : \"mq-lib\",\"hostaddr\" : \"127.0.0.1\"}"))
-
-(defn- publish
+(defn- test-msg
   []
-  (try
-    (do
-      (log/info "sending status")
-      (send-it test-msg))
-    (catch Throwable t
-      (log/error t))))
+  (-> {:app "mq-lib" :hostaddr "127.0.0.1" :ts (System/currentTimeMillis)}
+      (pr-str)
+      (.getBytes)))
 
-(defn- start-publisher
-  []
-  (run-daemon :stat-pub publish 5))
+(defn- publish-loop!
+  [control]
+  (thread
+    (loop []
+      (let [[val port] (alts!! [control (timeout 5000)])]
+        (when-not (= port control)
+          (send-it (test-msg))
+          (recur))))))
 
-(defn- stop-publisher
-  []
-  (stop-daemon :stat-pub))
-
-
-;; ----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 ;; Consumer stuff
-;; ----------------------------------------------------------------------------
 
 (defn- consume
   [message]
@@ -84,33 +52,39 @@
   []
   (mq/unsubscribe :system-status))
 
-;; ----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 ;; Standalone app
-;; ----------------------------------------------------------------------------
+
+(defn- make
+  []
+  (atom {:control-ch (chan)}))
 
 (defn- start
-  []
+  [this]
   (log/info "Starting mq.main.")
   (mq/start)
+  (publish-loop! (:control-ch @this))
   (start-consumer)
-  (start-publisher)
   :started)
 
 (defn- stop
-  []
+  [this]
   (log/info "Stopping mq.main.")
-  (stop-publisher)
+  (when-let [c (:control-ch @this)]
+    (close! c)
+    (swap! this assoc :control-ch (chan)))
   (stop-consumer)
   (mq/stop))
 
-;; ----------------------------------------------------------------------------
+;;-----------------------------------------------------------------------------
 
 (defn -main
   [& args]
-  (log/info "Hello MQ Test App")
-  (shutdown-hook "mq.main.shutdown" (fn []
-                                      (stop)
-                                      (release-lock)))
-  (start)
-  (acquire-lock)
-  (log/info "Done."))
+  (log/info "hello mq test app")
+  (let [lock (promise)
+        app (make)]
+    (shutdown-hook (fn [] (stop app)))
+    (shutdown-hook (fn [] (deliver lock :done)))
+    (start app)
+    (deref lock)
+    (System/exit 0)))
